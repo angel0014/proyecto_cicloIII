08d8b6585132349e937abd99511e17b2fe4ba5af
ANGELLO PERILLA <angello.perilla.mt@correo.usa.edu.co>
2021-11-05 09:46:29 -0500

    Reto 5 para cargue en plataforma completamente funcional

--- RETOS/src/main/java/mintic/cicloiii/retos/RetosApplication.java
+++ RETOS/src/main/java/mintic/cicloiii/retos/RetosApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Reto3Application {
+public class RetosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Reto3Application.class, args);
+		SpringApplication.run(RetosApplication.class, args);
 	}
 
 }
