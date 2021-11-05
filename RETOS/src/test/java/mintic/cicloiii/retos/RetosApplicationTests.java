3dc52070272ab3c5b7908d98ebb0f48f8ebbc05a
ANGELLO PERILLA <angello.perilla.mt@correo.usa.edu.co>
2021-11-05 08:36:29 -0500

    Reto 5 final para cargue

--- RETOS/src/test/java/mintic/cicloiii/retos/RetosApplicationTests.java
+++ RETOS/src/test/java/mintic/cicloiii/retos/RetosApplicationTests.java
@@ -1,10 +1,10 @@
-package mintic.cicloiii.reto3;
+package mintic.cicloiii.retos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Reto3ApplicationTests {
+class RetosApplicationTests {
 
 	@Test
 	void contextLoads() {
